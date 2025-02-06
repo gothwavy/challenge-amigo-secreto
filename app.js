954dc51c5a117caa829e6ca92d308fd1cbdfbe5e
gothwavy <ryanalmeidadev@gmail.com>
2025-02-06 03:00:22 -0300

    Implementando adicao de amigos a lista

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// Criando array para armazenar os nomes dos participantes.
+participantesSorteio = [];
+
+// Função para adicionar amigos a lista.
+function adicionarAmigo() {
+  // Verificando o input que contem o nome do amigo.
+  let entradaUsuario = document.getElementById(`amigo`);
+  let amigoInserido = entradaUsuario.value;
+  
+  // Verificando se o valor de entrada esta vazio.
+  if (amigoInserido == ``) {
+    alert(`Por favor, insira um nome.`);
+  }
+  else {
+    participantesSorteio.push(amigoInserido);
+  }
+
+  // Limpando o input
+  document.getElementById(`amigo`).innerHTML=``;
+}
+
